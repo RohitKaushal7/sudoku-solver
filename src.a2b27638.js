c52f430fbce576caa2b492dae7065d06cc370664
RohitKaushal7 <rohitkk252550@gmail.com>
2020-04-08 11:22:59 +0530

    Updates

--- src.a2b27638.js
+++ src.a2b27638.js
@@ -30505,7 +30505,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36677" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45205" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
